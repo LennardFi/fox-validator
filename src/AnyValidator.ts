c9ac393d80cad1c4f40abda9e73bd86db52ae7da
LennardFi <info@lennard-fickus.de>
2020-10-13 11:44:30 +0200

    added jsdoc for anyvalidator

--- src/AnyValidator.ts
+++ src/AnyValidator.ts
@@ -1,5 +1,8 @@
 import { ValidatorBase } from "./ValidatorBase"
 
+/**
+ * A validator that returns always true
+ */
 export default class AnyValidator extends ValidatorBase<unknown> {
     constructor() {
         super()
